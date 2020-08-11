90ea8542f6242c74943887656445769971ae0690
hannahturer <69254660+hannahturer@users.noreply.github.com>
2020-08-10 23:24:39 -0400

    Add files via upload

---
+++ ToDoListProject/ToDoListProject/ToDoListProject/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoListProject
+//
+//  Created by Hannah Turer on 8/10/20.
+//  Copyright © 2020 Hannah Turer. All rights reserved.
+//
+
+import UIKit
+
+class ToDoClass {
+    var description = ""
+    var important = false
+}
